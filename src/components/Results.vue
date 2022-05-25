5d15cbb20041fcecf4f86486c673d2d889da77ff
Rafael Lima <rafaa99@gmail.com>
2022-05-25 10:46:03 -0300

    add styles

---
+++ src/components/Results.vue
@@ -0,0 +1,39 @@
+<template>
+  <p>Tempo de reação: {{ score }} milissegundos</p>
+  <p :class="this.score < 150 ? 'king' : ''" class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 150) this.rank = "👑👑👑 Incomparável!! 👑👑👑";
+    else if (this.score < 200)
+      this.rank = "🔥🔥🔥TÁ PEGANDO FOGO BICHO!!🔥🔥🔥 ";
+    else if (this.score < 250) this.rank = "😱😱 Você é muito rápido(a) 😱😱";
+    else if (this.score < 500)
+      this.rank = "Tá bom... mas dá pra melhorar!! 🏃🏃🏃 ";
+    else if (this.score < 1000) this.rank = "Devagar e sempre né ... kkkk 🐌🐌";
+    else if (this.score < 2500)
+      this.rank = "Você sabe onde fica o mouse?? 😂😂😂😂";
+    else this.rank = "";
+  },
+};
+</script>
+
+<style>
+.king {
+  font-size: 2rem;
+  font-weight: bold;
+}
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
